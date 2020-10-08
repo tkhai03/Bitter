84a1c94b44fb628998db3f9f089d49df9b2df8a2
Thang Khai <tnkhai94@gmail.com>
2020-10-08 14:44:20 -0600

    controller.js

--- server/Controller.js
+++ server/Controller.js
@@ -1,12 +1,44 @@
-const getPosts = async(db) => {
+const getAllPosts = async(db) => {
     const posts = await db.get_posts()
     return posts
 }
 
 module.exports = {
-    getPosts: async(req, res) => {},
-    addPost: async(req, res) => {},
-    editPost: async(req, res) => {},
-    deletePost: async(req, res) => {},
-    followingPosts: async(req,res) => {}
+    getPosts: async(req, res) => {
+        const db = req.app.get('db')
+        const posts = await getAllPosts(db)
+        res.status(200).send(posts)
+    },
+    addPost: async(req, res) => {
+        const db = req.app.get('db')
+        const{id} = req.session.user
+        const{content} = req.body
+        
+        await db.add_post([id, content])
+
+        const posts = await getAllPosts(db)
+        res.status(200).send(posts)
+    },
+    editPost: async(req, res) => {
+        const db = req.app.get('db')
+        const {content} = req.body
+        const {post_id} = req.params
+
+        await db.edit_post([content, post_id])
+
+        const posts = await getAllPosts(db)
+        res.status(200).send(posts)
+    },
+    deletePost: async(req, res) => {
+        const db = req.app.get('db')
+        const {post_id} = req.params
+
+        await db.delete_post([post_id])
+
+        const posts = await getAllPosts(db)
+        res.status(200).send(posts)
+    },
+    followingPosts: async(req,res) => {
+        const db = req.app.get('db')
+    }
 }
