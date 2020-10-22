2f548762f08a86244fcc2030b51e7b6937185958
Thang Khai <tnkhai94@gmail.com>
2020-10-22 10:54:26 -0600

    landing responsive view

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Landing.css'
 import bitterLogo from './bitter-chat-background.jpg'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Register from '../Register/Register'
 import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
 
@@ -12,28 +12,30 @@ function Landing() {
 
     return (
         <div className="Landing">
-            <img className='bitterLogo'src={bitterLogo} alt='logo' />
-            <div className='posterText'>
-                <h1 className='posterMsg'>Follow your interests.</h1>
-                <h1 className='posterMsg'>Hear what people are talking about.</h1>
-                <h1 className='posterMsg'>Join the conversation</h1>
+            <div className='invisLanding'>
+                <img className='bitterLogo' src={bitterLogo} alt='logo' />
+                <div className='posterText'>
+                    <h1 className='posterMsg'>Follow your interests.</h1>
+                    <h1 className='posterMsg'>Hear what people are talking about.</h1>
+                    <h1 className='posterMsg'>Join the conversation</h1>
+                </div>
             </div>
             <div className='auth_container'>
-                <SentimentDissatisfiedIcon className='landing_bitterIcon'/>
+                <SentimentDissatisfiedIcon className='landing_bitterIcon' />
                 <div className='loginText'>
                     <h1 className='loginMsg'>See what's happening in the world right now</h1>
                 </div>
                 <div className='button-container'>
 
-                    <button className='button1' onClick= {() => setRegister(true)}>Sign up</button>
+                    <button className='button1' onClick={() => setRegister(true)}>Sign up</button>
 
                     <Link to='/auth'>
-                    <button className='button2'>Log in</button>                  
+                        <button className='button2'>Log in</button>
                     </Link>
                 </div>
             </div>
 
-            {register ? <Register/> : null}
+            {register ? <Register /> : null}
 
         </div>
     )
